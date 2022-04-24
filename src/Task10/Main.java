fd243caf31ea5a25c4f40db86070f4b4db5c656c
Hungstlenut <hustlenut@protonmail.com>
2022-04-24 23:18:33 +0200

    cleanup

--- src/Task10/Main.java
+++ src/Task10/Main.java
@@ -1,21 +1,60 @@
 package Task10;
+/*
+- Hashset
 
+- Passing objects as parameters
+- Testing constructor methods
+ */
 import java.util.HashSet;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        Program p = new Program();
+        //using the custom constructor that has to pass in 2 parameters!
+        Person person1 = new Person("Atle Antonsen", 20);
+        //this method takes in only methods from Person class as specified below.
+        //Passing in an object means also that you pass in the WHOLE class of that object.
+        Program add1 = new Program(person1);
+
+        //making a new person/object using the default constructor and passing it into Program.
+        Person person2 = new Person();
+        person2.setName("Rudolf");
+        person2.setAge(30);
+
+        Program add2 = new Program(person2);
+
+        //adding another person using the method in Program instead of the constructor
+        Person person3 = new Person("Levi", 25);
+
+        Program add3 = new Program();
+        //had to make an own method, because the others were constructors...
+        add3.addPerson(person3);
+
+        //lets check duplicates
+        Person person4 = new Person("Atle Antonsen", 20);
+        Program add4 = new Program(person4);
+
     }
 }
 
 class Program {
+    //this parameter is of the type class Person, that means I HAVE to use an object that comes
+    //from this class.
+    HashSet<Person> p = new HashSet<>();
 
-    public Program(String name, int age){
-        name =
-        HashSet<String> n = new HashSet<>();
+    public Program(Person nameAndAge){
+        p.add(nameAndAge);
 
+        System.out.println(p);
+    }
+    public Program(){
+        System.out.println("I'm a default constructor!");
     }
 
+    public void addPerson(Person person){
+        p.add(person);
+        System.out.println(p);
+    }
 }
 
 class Person {
@@ -39,13 +78,18 @@ class Person {
         return name;
     }
 
+    public Person (){
+        System.out.println("Name is " + getName() + ", age is" + getAge());
+    }
+
     public Person (String name, int age){
         this.name = name;
         this.age = age;
+
     }
 
     public String toString(){
-        return "Name: " + name + "\n age:" + age;
+        return "Name: " + name + ", hash:" + this.name.hashCode() + "\n age:" + age;
     }
 
 }
